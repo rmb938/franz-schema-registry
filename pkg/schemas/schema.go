727a73d1ad18301559849ca6b88d8d41da4792be
Ryan Belgrave <ryan@belgrave.me>
2023-04-01 23:07:24 -0500

    avro reference naming override checks need to recurse

--- pkg/schemas/schema.go
+++ pkg/schemas/schema.go
@@ -37,11 +37,20 @@ func ParseSchema(rawSchema string, schemaType SchemaType, references map[string]
 			return nil, fmt.Errorf("error parsing avro schema: %w", err)
 		}
 
-		// make sure our schema's name isn't the same as a reference
-		//  while self-references is allowed in avro, it is not allowed in schema registry
+		// make sure we aren't doing weird naming things
 		if namedSchema, ok := avroSchema.(avro.NamedSchema); ok {
-			if _, ok := references[namedSchema.Name()]; ok {
-				return nil, fmt.Errorf("can't redefine: %s", namedSchema.Name())
+			// make sure our schema's name isn't the same as a reference
+			if name, ok := isAvroOverrideReferenceName(references, namedSchema); ok {
+				return nil, fmt.Errorf("can't redefine: %s", name)
+			}
+
+			// check if self referencing
+			//  while self-references are allowed per avro spec it is not allowed in schema registry
+			if recordSchema, ok := avroSchema.(*avro.RecordSchema); ok {
+				if isAvroSelfReferencing(recordSchema.Name(), recordSchema) {
+					fmt.Println(avroSchema.String())
+					return nil, fmt.Errorf("can't self-reference: %s", namedSchema.Name())
+				}
 			}
 		}
 
@@ -53,4 +62,48 @@ func ParseSchema(rawSchema string, schemaType SchemaType, references map[string]
 	}
 
 	return parsedSchema, nil
+}
+
+func isAvroOverrideReferenceName(references map[string]string, namedSchema avro.NamedSchema) (string, bool) {
+	if _, ok := references[namedSchema.Name()]; ok {
+		return namedSchema.Name(), true
+	}
+
+	if recordSchema, ok := namedSchema.(*avro.RecordSchema); ok {
+		for _, field := range recordSchema.Fields() {
+			if refSchema, ok := field.Type().(*avro.RefSchema); ok {
+				if subNamedSchema, ok := refSchema.Schema().(avro.NamedSchema); ok {
+					return isAvroOverrideReferenceName(references, subNamedSchema)
+				}
+			}
+			if subRecord, ok := field.Type().(*avro.RecordSchema); ok {
+				return isAvroOverrideReferenceName(references, subRecord)
+			}
+		}
+	}
+
+	return "", false
+}
+
+func isAvroSelfReferencing(name string, recordSchema *avro.RecordSchema) bool {
+	for _, field := range recordSchema.Fields() {
+		if refSchema, ok := field.Type().(*avro.RefSchema); ok {
+			if namedSchema, ok := refSchema.Schema().(avro.NamedSchema); ok {
+				if namedSchema.Name() == name {
+					fmt.Printf("Field %s is a reference to %s\n", field.Name(), namedSchema.Name())
+					return true
+				}
+
+				if subRecord, ok := namedSchema.(*avro.RecordSchema); ok {
+					return isAvroSelfReferencing(name, subRecord)
+				}
+			}
+		}
+
+		if subRecord, ok := field.Type().(*avro.RecordSchema); ok {
+			return isAvroSelfReferencing(name, subRecord)
+		}
+	}
+
+	return false
 }
