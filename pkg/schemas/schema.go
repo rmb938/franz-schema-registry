0dea106eb90ac4f64adb97928be87fdc2294a6b2
Ryan Belgrave <ryan@belgrave.me>
2023-04-01 22:30:02 -0500

    add comment about avro self-references being blocked

--- pkg/schemas/schema.go
+++ pkg/schemas/schema.go
@@ -37,6 +37,8 @@ func ParseSchema(rawSchema string, schemaType SchemaType, references map[string]
 			return nil, fmt.Errorf("error parsing avro schema: %w", err)
 		}
 
+		// make sure our schema's name isn't the same as a reference
+		//  while self-references is allowed in avro, it is not allowed in schema registry
 		if namedSchema, ok := avroSchema.(avro.NamedSchema); ok {
 			if _, ok := references[namedSchema.Name()]; ok {
 				return nil, fmt.Errorf("can't redefine: %s", namedSchema.Name())
