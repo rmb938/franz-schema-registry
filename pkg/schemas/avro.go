68d78bdf8687dfd34af5a94926c1d0ab53520e0a
Ryan Belgrave <ryan@belgrave.me>
2023-04-04 07:18:15 -0500

    confluent sr doesn't guarantee compatibility of encoded data

--- pkg/schemas/avro.go
+++ pkg/schemas/avro.go
@@ -20,7 +20,10 @@ func (s *ParsedAvroSchema) IsBackwardsCompatible(previousSchema ParsedSchema) (b
 
 	// This compatible check checks compatibility of the schema itself but does not guarantee compatibility of the encoded data
 	// i.e. if record fields, enums, unions change order the encoded data is no longer compatible
-	// we need to replace this with that confluent SR does if it does something different
+	// Confluent SR: https://github.com/confluentinc/schema-registry/blob/9ef76b4a1373f50a505162e72cffcbfd3dd2fee3/client/src/main/java/io/confluent/kafka/schemaregistry/avro/AvroSchema.java#LL327C40-L327C40
+	// Java Avro Library: https://github.com/apache/avro/blob/916a09ce852769b9172882957e4b766b2970dd52/lang/java/avro/src/main/java/org/apache/avro/SchemaCompatibility.java#LL262C50-L262C50
+	// it looks like the Java Avro Library also doesn't guarantee compatibility of the encoded data
+	// so this is probably ok
 	compatibilityErr := schemaCompat.Compatible(previousAvroSchema.avroSchema, s.avroSchema)
 	if compatibilityErr != nil {
 		return false, nil
